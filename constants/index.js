514d20e6a4ab72224a2b2841dba5b3e0a0a675e9
emre-crg <vrtemre@gmail.com>
2021-01-18 11:42:36 +0300

    useHook, postcss-custom-media

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+export default {
+  MOBILE_SIZE: 480,
+  TABLET_SIZE: 980,
+  DESKTOP_SIZE: 1270,
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
-import Button from '../components/navigation-button'
-import Navigation from '../components/navigation'
+import CONST from '../constants/index'
+import Layout from '../components/layout'
+import Sidebar from '../components/col-sidebar'
+import Main from '../components/col-main'
+import Extra from '../components/col-extra'
+import useWindowSize from '../hooks/useWindowSize'
 
 function HomePage() {
+  const size = useWindowSize()
+
   return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
-    </div>
+    <Layout>
+      {size.width > CONST.MOBILE_SIZE && (
+        <Sidebar flat={size.width < CONST.DESKTOP_SIZE}>Sidebar</Sidebar>
+      )}
+      <Main>{JSON.stringify(size)}</Main>
+      {size.width > CONST.TABLET_SIZE && <Extra>Extra</Extra>}
+    </Layout>
   )
 }
 
